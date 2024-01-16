d00f820bd7801b1d3c0e9d56833987b1277ca0c5
Reinaldo Ferreira <reinaldojuly@gmail.com>
2024-01-16 17:58:35 -0300

    Update pgcorte.php

--- pgcorte.php
+++ pgcorte.php
@@ -9,12 +9,13 @@
 <body>
     <div class="block-page">
         <div class="content">
-            <img src="img/aviso.jpg" alt="Imagem de Bloqueio">
+            <img src="https://vl56.github.io/pgcorte-mk/img/aviso.jpg" alt="Imagem de Bloqueio">
             <h1>Entre em contato com nossa Central : <a class="botao" href="http:wa.me/5581981795111" target="_blank" >(81) 90000 0000</a></h1>
             <p>Segunda via de  
                 <a class="botao" href="http://198.50.183.161:85/central/login.hhvm">Boleto</a>  em nossa central do cliente.
             </p>
         </div>
+
     </div>
 </body>
 </html>
